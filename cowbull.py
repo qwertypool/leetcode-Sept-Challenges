01933ac7e4f4b9b92d32853e101e68f02070dce4
Deepa Pandey <deepapandey364@gmail.com>
2020-09-11 02:50:39 +0530

    sept11

--- cowbull.py
+++ cowbull.py
@@ -1,4 +1,9 @@
-# You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+# You are playing the following Bulls and Cows game with your friend: 
+#You write down a number and ask your friend to guess what the number is.
+# Each time your friend makes a guess, you provide a hint that indicates
+# how many digits in said guess match your secret number exactly in both digit and position (called "bulls")
+# and how many digits match the secret number but locate in the wrong position (called "cows").
+# Your friend will use successive guesses and hints to eventually derive the secret number.
 
 # Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. 
 
